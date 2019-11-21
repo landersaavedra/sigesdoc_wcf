fb6847ff5a40d358b73510bb575f8dd6d17eadd6
lander saavedra <landersaavedra@gmail.com>
2019-11-21 12:24:19 -0500

    commit

---
+++ WCF_SIGESDOC_SANIPES/WCF_SIGESDOC_SANIPES/App_Code/Data/Recepcion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Recepcion
+/// </summary>
+public class Recepcion
+{
+    public Recepcion()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
