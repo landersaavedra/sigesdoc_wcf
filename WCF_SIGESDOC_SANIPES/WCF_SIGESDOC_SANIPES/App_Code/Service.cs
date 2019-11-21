2e2e4eb4f0654929443d9f4688dacf6a3adbf1db
lander saavedra <landersaavedra@gmail.com>
2019-11-21 11:53:50 -0500

    commit

--- WCF_SIGESDOC_SANIPES/WCF_SIGESDOC_SANIPES/App_Code/Service.cs
+++ WCF_SIGESDOC_SANIPES/WCF_SIGESDOC_SANIPES/App_Code/Service.cs
@@ -6,6 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
+
 // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
 public class Service : IService
 {
