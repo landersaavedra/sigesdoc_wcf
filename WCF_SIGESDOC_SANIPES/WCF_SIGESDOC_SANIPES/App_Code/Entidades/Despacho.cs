3c5cd5ebc7b7e2d480ffc1453a0c859267bb87e2
lander saavedra <landersaavedra@gmail.com>
2019-11-21 11:56:49 -0500

    commit Entidades Despacho

---
+++ WCF_SIGESDOC_SANIPES/WCF_SIGESDOC_SANIPES/App_Code/Entidades/Despacho.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Despacho
+/// </summary>
+public class Despacho
+{
+    public Despacho()
+    {
+        //
+        // TODO: Agregar aquí la lógica del constructor
+        //
+    }
+}
